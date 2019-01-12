32e89f95d6311dada01376b937ac5148dda63f04
Nelson Gonzalez <nelglezfl@gmail.com>
2019-01-12 15:27:06 -0500

    Setup outlets for all things in the Signup scene

--- Jchat/Jchat/SignUpViewController.swift
+++ Jchat/Jchat/SignUpViewController.swift
@@ -9,22 +9,39 @@
 import UIKit
 
 class SignUpViewController: UIViewController {
+    
+    @IBOutlet weak var titleTextLabel: UILabel!
+    
+    @IBOutlet weak var avatar: UIImageView!
+    
+    @IBOutlet weak var fullNameContainerView: UIView!
+    
+    @IBOutlet weak var fullNameTextField: UITextField!
+    
+    @IBOutlet weak var emailContainerView: UIView!
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var passwordContainerView: UIView!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var signUpButton: UIButton!
+    
+    @IBOutlet weak var signInButton: UIButton!
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        setupUI()
+      
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func setupUI() {
+        
     }
-    */
 
 }
