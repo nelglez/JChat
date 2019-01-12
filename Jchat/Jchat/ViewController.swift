6c1b13cf070c4ec1a8d80a4aa3257ebc3adbebce
Nelson Gonzalez <nelglezfl@gmail.com>
2019-01-12 13:09:05 -0500

    Add Xcode Project

---
+++ Jchat/Jchat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Jchat
+//
+//  Created by Nelson Gonzalez on 1/12/19.
+//  Copyright © 2019 Nelson Gonzalez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
